83fe4b35f3e11eae1194b810b14295d3becc66cf
Steve Boyer <stephen.a.boyer@gmail.com>
2023-08-23 12:38:39 -0400

    Render data object onto Parts component via useParams()

--- src/pages/Parts.js
+++ src/pages/Parts.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 import "antd/dist/reset.css"
 import "../App.css"
 import { seriesParts } from "../config/data.js"
@@ -9,9 +10,17 @@ function Parts() {
   console.log("seriesParts in Parts.js: ", seriesParts)
   console.log(location.pathname)
 
+
+  const { slug } = useParams()
+  const part = seriesParts[slug]
+
+
+  const { title, rumble } = part
+
   return (
     <div>
-      <p>PARTS</p>
+      <p>Parts Component</p>
+      <h3>{title}</h3>
 
 
     </div>
